8e745edc6500564d6550d1c40f5fe963226dc589
ethayu <ethayu1@gmail.com>
2020-02-17 15:57:39 -0800

    updated registration activity

--- app/src/main/java/com/bravo/bravoapp/RegisterActivity.java
+++ app/src/main/java/com/bravo/bravoapp/RegisterActivity.java
@@ -1,11 +1,33 @@
 package com.bravo.bravoapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText rEmailEdit, rPasswordEdit, rWait;
+    Button rRegisterBtn;
+    ProgressBar progressBar;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +39,59 @@ public class RegisterActivity extends AppCompatActivity {
 
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setDisplayShowHomeEnabled(true);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        RelativeLayout layout = findViewById(R.id.display);
+
+        rEmailEdit = findViewById(R.id.rEmailEdit);
+        rPasswordEdit = findViewById(R.id.rPasswordEdit);
+        rRegisterBtn = findViewById(R.id.rRegisterBtn);
+        progressBar = new ProgressBar(this);
+
+        rRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = rEmailEdit.getText().toString().trim();
+                String password = rPasswordEdit.getText().toString().trim();
+
+                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    rEmailEdit.setError("Invalid Email");
+                    rEmailEdit.setFocusable(true);
+                } else if (password.length() < 4) {
+                    rPasswordEdit.setError("Password length must be greater than 4");
+                    rPasswordEdit.setFocusable(true);
+                } else {
+                    registerUser(email, password);
+                }
+            }
+        });
+    }
+
+    private void registerUser(String email, String password) {
+        rWait.setText("Making Your Account!");
+        RelativeLayout.LayoutParams params1 = new
+                RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        params1.addRule(RelativeLayout.ABOVE, progressBar.getId());
+        rWait.setLayoutParams(params1);
+        progressBar.setIndeterminate(false);
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                progressBar
+            }
+        })
+
     }
 
     @Override
